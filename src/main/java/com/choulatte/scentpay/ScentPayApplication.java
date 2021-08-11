6d1a48d78ecf32590eceeefc41649586771644f4
lattechiffon <lattechiffon@gmail.com>
2021-08-12 00:18:06 +0900

    apply eureka client

--- src/main/java/com/choulatte/scentpay/ScentPayApplication.java
+++ src/main/java/com/choulatte/scentpay/ScentPayApplication.java
@@ -3,9 +3,11 @@ package com.choulatte.scentpay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableCaching
+@EnableDiscoveryClient
 @EnableSwagger2
 @SpringBootApplication
 public class ScentPayApplication {
